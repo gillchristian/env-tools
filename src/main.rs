1f2e0be697fccd920f34b8938b9306b24a663a36
gillchristian <gillchristiang@gmail.com>
2023-01-06 13:54:47 +0100

    List of versions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::path::Path;
 use std::process::Command;
 
-// TODO:
-// - Add subcommands for other purposes
-// - Take parameters to support arbitrary versions without changing the source
-fn main() {
+// TODO: Take parameters to support arbitrary versions without changing the source
+fn stack_path () {
     let output = Command::new("sh")
         .arg("-c")
         .arg("stack path --compiler-tools-bin")
@@ -17,11 +15,21 @@ fn main() {
     // The Stack compiler tools looks something like
     // ~/.stack/compiler-tools/x86_64-linux-tinfo6/<ghc version>/bin
     // So we drop the `/bin` and `/<ghc-version>` directories
-    let stack_root = Path::new(stack_root.trim()).parent().unwrap().parent().unwrap();
+    let stack_root = Path::new(stack_root.trim())
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap();
 
     // And then add a few other ones that we one
-    let a = stack_root.clone().join(Path::new("ghc-9.2.5/bin"));
-    let b = stack_root.clone().join(Path::new("ghc-8.10.7/bin"));
+    let versions = [Path::new("ghc-9.2.5/bin"), Path::new("ghc-8.10.7/bin")];
 
-    println!("{}:{}", a.to_str().unwrap(), b.to_str().unwrap());
+    let paths = versions.map(|p| stack_root.clone().join(p).to_str().unwrap());
+
+    println!("{}", paths.join(":"));
+}
+
+// TODO: Add subcommands for other purposes
+fn main() {
+    stack_path()
 }
